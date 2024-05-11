0a02658b0ddd99530243eac516df20953809c251
faizanpro77 <faizanshikh7798@gmail.com>
2024-05-11 21:28:54 +0530

    Initial Commit

---
+++ ConflictMerge2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConflictMerge2
+//
+//  Created by shaikh faizan on 11/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
